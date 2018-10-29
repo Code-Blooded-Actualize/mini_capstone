0a79e0e39e616309f740cd957af1092521aec187
JackMarx <seriousfools@gmail.com>
2018-10-29 15:57:56 -0500

    frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,7 +39,7 @@ ProductCategory.create!([
   {product_id: 4, category_id: 1},
   {product_id: 2, category_id: 2},
   {product_id: 3, category_id: 2},
-  {product_id: 9, category_id: 2},
+  {product_id: 6, category_id: 2},
   {product_id: 4, category_id: 2},
   {product_id: 6, category_id: 2},
   {product_id: 3, category_id: 3},
